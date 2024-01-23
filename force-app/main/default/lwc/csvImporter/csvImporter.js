e771da09a739bfd62bbca78a79751637794b427c
James Wygocki <psychomanical@gmail.com>
2024-01-22 23:10:43 -0600

    Imported apex method to return CSV from upload and added basic parsing of CSV

--- force-app/main/default/lwc/csvImporter/csvImporter.js
+++ force-app/main/default/lwc/csvImporter/csvImporter.js
@@ -1,4 +1,5 @@
 import { LightningElement } from 'lwc';
+import getCSV from '@salesforce/apex/CSVImporterController.getCSV';
 
 export default class CsvImporter extends LightningElement {
     contentVersionId;
@@ -11,9 +12,19 @@ export default class CsvImporter extends LightningElement {
         return this.contentVersionId === undefined;
     }
 
+    handleMapButtonClick(event){
+        getCSV({ contentVersionId: this.contentVersionId})
+        .then(result =>{
+            let rowArr = result.split('\r\n');
+        })
+        .catch(error => {
+            console.log(error);
+        });
+    }
+
     handleUploadFinished(event) {
         const uploadedFiles = event.detail.files;
-        
+
         if(uploadedFiles && uploadedFiles[0].contentVersionId != null){
             this.contentVersionId = uploadedFiles[0].contentVersionId;
         }
