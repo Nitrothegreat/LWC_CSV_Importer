cbd453d6c1a6b91c06b31af75b09d511d08ac1c0
James Wygocki <psychomanical@gmail.com>
2024-01-22 22:34:07 -0600

    Added file upload and properties of filed uploader

--- force-app/main/default/lwc/csvImporter/csvImporter.js
+++ force-app/main/default/lwc/csvImporter/csvImporter.js
@@ -1,3 +1,12 @@
 import { LightningElement } from 'lwc';
 
-export default class CsvImporter extends LightningElement {}
+export default class CsvImporter extends LightningElement {
+    get acceptedFormats() {
+        return ['.csv'];
+    }
+
+    handleUploadFinished(event) {
+        const uploadedFiles = event.detail.files;
+        console.log(JSON.stringify(uploadedFiles, null, 4));
+    }
+}
