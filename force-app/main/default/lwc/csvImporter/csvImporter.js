4a6222c335e8e810e4b1e169d946eeb125ffbe45
James Wygocki <psychomanical@gmail.com>
2024-01-22 22:57:09 -0600

    Added a map fields button for future need and enabled storing the content version id of the file uploaded

--- force-app/main/default/lwc/csvImporter/csvImporter.js
+++ force-app/main/default/lwc/csvImporter/csvImporter.js
@@ -1,12 +1,31 @@
 import { LightningElement } from 'lwc';
 
 export default class CsvImporter extends LightningElement {
+    contentVersionId;
+
     get acceptedFormats() {
         return ['.csv'];
     }
 
+    get disableMapFieldsButton(){
+        return this.contentVersionId === undefined;
+    }
+
     handleUploadFinished(event) {
         const uploadedFiles = event.detail.files;
-        console.log(JSON.stringify(uploadedFiles, null, 4));
+        
+        if(uploadedFiles && uploadedFiles[0].contentVersionId != null){
+            this.contentVersionId = uploadedFiles[0].contentVersionId;
+        }
     }
 }
+
+// [
+//     {
+//         "name": "Test.csv",
+//         "documentId": "[REDACTED]",
+//         "contentVersionId": "[REDACTED]",
+//         "contentBodyId": "[REDACTED]",
+//         "mimeType": "text/csv"
+//     }
+// ]
